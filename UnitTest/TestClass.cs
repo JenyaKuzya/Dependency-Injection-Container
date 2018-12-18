50903a946b343a88b9e2bed3845f92cf0ea559c7
Евгений <jenya.kuzya@mail.ru>
2018-12-18 15:40:00 +0300

    tests

---
+++ UnitTest/TestClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public interface IWrong { }
+    public abstract class WrongClass1 : IWrong { }
+    public class NotWrong : WrongClass1 { }
+
+    public interface IFoo { }
+    public class Foo : IFoo { }
+    public class Foo2 : IFoo { }
+
+    public class NotWrongInFoo : IFoo
+    {
+        public NotWrongInFoo(WrongClass1 notWrong)
+        {
+            this.notWrong = (NotWrong)notWrong;
+        }
+
+        public NotWrong notWrong { get; set; }
+    }
+
+    public class GenClass<T>
+    {
+        T value;
+    }
+
+    public interface IService<T> { }
+    public class ServiceImpl<T> : IService<T> { }
+}
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DependencyInjectionContainer;
+using System.Linq;
 
 namespace UnitTest
 {
@@ -7,8 +9,89 @@ namespace UnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SingletonTest()
         {
+            var config = new DependenciesConfiguration();
+            config.Register(typeof(IFoo), typeof(Foo), true);
+            var provider = new DependencyProvider(config);
+            IFoo foo1 = provider.Resolve<IFoo>();
+            IFoo foo2 = provider.Resolve<IFoo>();
+            Assert.AreSame(foo1, foo2);
+        }
+
+        [TestMethod]
+        public void NotSingletonTest()
+        {
+            var config = new DependenciesConfiguration();
+            config.Register(typeof(IFoo), typeof(Foo), false);
+            var provider = new DependencyProvider(config);
+            IFoo foo1 = provider.Resolve<IFoo>();
+            IFoo foo2 = provider.Resolve<IFoo>();
+            Assert.AreNotEqual(foo1, foo2);
+        }
+
+        [TestMethod]
+        public void SomeRealizationsTest()
+        {
+            var config = new DependenciesConfiguration();
+            config.Register(typeof(IFoo), typeof(Foo), false);
+            config.Register(typeof(IFoo), typeof(Foo2), false);
+            var provider = new DependencyProvider(config);
+            var objects = provider.ResolveAll<IFoo>();
+            Assert.AreEqual(objects.Count(), 2);
+        }
+
+        [TestMethod]
+        public void ResolveGenTypeTest()
+        {
+            var config = new DependenciesConfiguration();
+            config.Register(typeof(IFoo), typeof(Foo));
+            config.Register(typeof(GenClass<IFoo>), typeof(GenClass<IFoo>));
+            var provider = new DependencyProvider(config);
+            GenClass<IFoo> obj = provider.Resolve<GenClass<IFoo>>();
+            Assert.IsNotNull(obj);
+        }
+
+        [TestMethod]
+        public void WrongRegisterTest()
+        {
+            var config = new DependenciesConfiguration();
+            config.Register(typeof(IWrong), typeof(WrongClass1));
+            var provider = new DependencyProvider(config);
+            IWrong obj = provider.Resolve<IWrong>();
+            Assert.IsNull(obj);
+        }
+
+        [TestMethod]
+        public void NotWrongRegisterTest()
+        {
+            var config = new DependenciesConfiguration();
+            config.Register(typeof(WrongClass1), typeof(NotWrong));
+            var provider = new DependencyProvider(config);
+            IWrong obj = provider.Resolve<WrongClass1>();
+            Assert.IsNotNull(obj);
+        }
+
+        [TestMethod]
+        public void NestedTest()
+        {
+            var config = new DependenciesConfiguration();
+            config.Register(typeof(WrongClass1), typeof(NotWrong));
+            config.Register(typeof(IFoo), typeof(NotWrongInFoo));
+            var provider = new DependencyProvider(config);
+            NotWrongInFoo obj = (NotWrongInFoo)provider.Resolve<IFoo>();
+            Assert.IsNotNull(obj.notWrong);
+        }
+
+        [TestMethod]
+        public void OpenGenericTest()
+        {
+            var config = new DependenciesConfiguration();
+            config.Register(typeof(IService<>), typeof(ServiceImpl<>));
+            config.Register(typeof(IFoo), typeof(Foo));
+            var provider = new DependencyProvider(config);
+            IService<Foo> obj = provider.Resolve<IService<Foo>>();
+            Assert.IsNotNull(obj);
         }
     }
 }
